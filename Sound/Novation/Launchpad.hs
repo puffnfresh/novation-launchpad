a1d30e7c1b978a00857fc7c4e1f95d994bcf54b3
Brian McKenna <bmckenna@atlassian.com>
2018-01-10 21:56:03 +1100

    Add some animation

--- Sound/Novation/Launchpad.hs
+++ Sound/Novation/Launchpad.hs
@@ -2,12 +2,15 @@
 
 module Sound.Novation.Launchpad where
 
+import           Control.Concurrent
+import           Control.Monad
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Except
 import           Data.Bits
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Char8      as Char8
 import           Data.Char
+import           Data.Foldable
 import           Data.Int
 import           Data.List
 import           Foreign.C.Types
@@ -48,6 +51,7 @@ data Brightness
 
 data Color
   = Color (Maybe Brightness) (Maybe Brightness)
+  deriving (Eq, Ord, Show)
 
 red :: Color
 red =
@@ -165,3 +169,43 @@ color (Color r g) =
 sendSysEx :: BS.ByteString -> PMStream -> IO PMError
 sendSysEx m n =
   writeSysEx n 0 ([chr 0xf0] ++ Char8.unpack m ++ [chr 0xf7])
+
+overTime :: Int -> (Int -> IO a) -> Launchpad -> IO ()
+overTime delay f l =
+  traverse_ (\i -> resetLaunchpad l *> f i *> threadDelay delay) [0..]
+
+plot :: Int -> (Int64 -> Int64) -> (Coord -> IO a) -> Launchpad -> IO ()
+plot delay f g l =
+  overTime delay (for_ [0..7] . h) l
+  where
+    h x n =
+      g . Coord n . f $ fromIntegral x + n
+
+wave :: Int -> Color -> Launchpad -> IO ()
+wave delay color l =
+  plot delay f (\c -> coordOn c color l) l
+  where
+    f n =
+      floor $ sin (fromIntegral n / 3) * 4 + 4
+
+drawByteString :: BS.ByteString -> Color -> Launchpad -> IO ()
+drawByteString bs color l =
+  for_ (zip [0..] $ BS.unpack bs) $ \(y, b) ->
+    for_ [0..7] $ \x ->
+      when (testBit b $ fromIntegral x) . void $
+        coordOn (Coord x y) color l
+
+scrollBetween :: Int -> BS.ByteString -> BS.ByteString -> Color -> Launchpad -> IO ()
+scrollBetween delay bs bs' color l =
+  for_ [0..7] $ \x -> do
+    resetLaunchpad l
+    drawByteString (BS.map (`shiftR` x) bs) color l
+    drawByteString (BS.map (`shiftL` (8 - x)) bs') color l
+    threadDelay delay
+
+scrollAll :: Int -> [BS.ByteString] -> Color -> Launchpad -> IO ()
+scrollAll delay (a:b:bs) color l = do
+  scrollBetween delay a b color l
+  scrollAll delay (b:bs) color l
+scrollAll _ bs color l =
+  for_ bs $ \b -> drawByteString b color l
